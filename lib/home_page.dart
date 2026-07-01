d4f85a26bbd39f21136cdee72fa7090dd4654b6d
Miguel <miguegv10@gmail.com>
2026-07-01 08:50:36 -0500

    Version 2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,15 +7,49 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "SENA",
-          style: TextStyle(fontSize: 32, color: Colors.yellow),
+        title: Center(
+          child: Text(
+            "SENA",
+            style: TextStyle(
+              fontSize: 32,
+              color: const Color.fromARGB(255, 3, 19, 6),
+            ),
+          ),
         ),
-        backgroundColor: Colors.blue,
+        backgroundColor: const Color.fromARGB(255, 60, 146, 10),
       ),
-      body: Text(
-        "Servicio Nacional de Aprendizaje - SENA",
-        style: TextStyle(fontSize: 32, color: Colors.blue),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Servicio Nacional de Aprendizaje - SENA",
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: const Color.fromARGB(255, 33, 243, 138),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Centro de Comercio y Servicios",
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: const Color.fromARGB(255, 209, 12, 12),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Analisis y Desarrollo de Sistemas"),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
